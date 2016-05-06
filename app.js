6e9dde7bdc5dcb6e34a88ee31e67a4790b78f537
Stanislav Jakuschevskij <Stas@giantBook.local>
2016-05-06 12:02:51 +0200

    added baths and refactored code

--- app.js
+++ app.js
@@ -1,133 +1,121 @@
 (function() {
     "use strict";
     
-    var taxiMarkers = [];
-    var visible = false;
+    // start initialize whn browser is ready
+    google.maps.event.addDomListener(window, 'load', init);
     
-    function initialize() {
-        // map properties
-        var myCenter = new google.maps.LatLng(48.208174, 16.373819);
-        var mapProp  = {
-            center: myCenter,
+    function init() {
+        var map;
+        var taxisSrc = 'http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TAXIOGD&srsName=EPSG:4326&outputFormat=json';
+        var bathSrc = 'http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BADESTELLENOGD&srsName=EPSG:4326&outputFormat=json';
+
+        // show map
+        map = mapSetup();
+        
+        // load and show taxis
+        getAndShowData(map, taxisSrc);
+        
+        // load and show baths
+        getAndShowData(map, bathSrc);
+    }
+    
+    function mapSetup() {
+        var mapProp = {};
+        var map     = {};
+        
+        mapProp  = {
+            center: new google.maps.LatLng(48.208174, 16.373819),
             zoom: 13,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             disableDefaultUI: true
         };
 
-        // map
-        var map = new google.maps.Map($('#map')[0], mapProp);
+        map = new google.maps.Map($('#map')[0], mapProp);
         
-        taxis(map);
-        
-        google.maps.event.addDomListener($("#taxis")[0], 'click', function() {
-            log(taxiMarkers.length);
-            for (var i in taxiMarkers) {
-                taxiMarkers[i].setVisible(visible);
-            }
-            
-            if(visible) {
-                visible = false;
-            } else {
-                visible = true;
-            }
-        });
-        
+        return map;
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
     
-    function taxis(map) {
-        // taxi json
-        var taxiSrc = 'http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TAXIOGD&srsName=EPSG:4326&outputFormat=json';
+    function getAndShowData(map, src) {
+        $.getJSON(src, jsonDataCb);
         
-        // Taxi data
-        $.getJSON(taxiSrc, taxiDataCb);
-        
-        function taxiDataCb(data) {
-            var marker;
-            var latLng;
-            var infoWindow;
-            var address;
-            var taxiPoints = data.features;
-            
-            for (var i in taxiPoints) {
-                latLng  = taxiPoints[i].geometry.coordinates[0];
-                address = taxiPoints[i].properties.ADRESSE;
-                marker  = new google.maps.Marker({
-                    position: new google.maps.LatLng(latLng[1], latLng[0]),
-                    map: map
-                });
-                taxiMarkers.push(marker);
-                
+        function jsonDataCb(data) {
+            var marker     = {};
+            var latLng     = [];
+            var infoWindow = {};
+            var iwContent  = '';
+            var points  = []; 
+            var markers = [];
+            var type    = '';
+
+            points = data.features;
+
+            // check case: baths or taxis?
+            if (points[0].id.indexOf('BAD') >= 0) {
+                type = 'baths';
+            } else {
+                type = 'taxis';
+            }
+
+            // create markers and info windows
+            for (var i in points) {
+                if(type === 'baths') {
+                    latLng  = points[i].geometry.coordinates;
+                    iwContent = points[i].properties.BEZEICHNUNG;
+
+                    marker  = new google.maps.Marker({
+                        position: new google.maps.LatLng(latLng[1], latLng[0]),
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                        map: map
+                    });
+                } else {
+                    latLng  = points[i].geometry.coordinates[0];
+                    iwContent = points[i].properties.ADRESSE;
+
+                    marker  = new google.maps.Marker({
+                        position: new google.maps.LatLng(latLng[1], latLng[0]),
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                        map: map
+                    });
+                }
+
                 marker.info = new google.maps.InfoWindow({
-                    content: address
+                    content: iwContent
                 });
-                
+
+                markers.push(marker);
+
+                // mouse events
                 google.maps.event.addListener(marker, 'mouseover', mouseOverHandler(map, marker));
                 google.maps.event.addListener(marker, 'mouseout', mouseOutHandler(marker));
             }
+
+            google.maps.event.addDomListener($("#" + type)[0], 'click', buttonClickHandler(markers));
         }
     }
-    
+       
     // event handlers
-    function taxiButtonHandler(map, myCenter) {
-//        for(var i in taxiMarkers) {
-//            taxiMarkers[i].setMap(null);
-//        }
-//
-        map.setZoom(15);
-        map.setCenter(myCenter);
+    function buttonClickHandler(markers) {
+        return function buttonClickHandler() {
+            for (var i in markers) {
+                markers[i].setVisible(!(markers[i].getVisible()));
+            }
+        };
     }
     
-    function clickHandler(mapForLoop, markerForLoop) {
-        return function clickHandlerReturn() {
-            markerForLoop.info.open(mapForLoop, markerForLoop);
-        };
-    }
-
-    function mouseOverHandler(mapForLoop, markerForLoop) {
+    function mouseOverHandler(map, marker) {
         return function mouseOverHandlerReturn() {
-            markerForLoop.info.open(mapForLoop, markerForLoop);
+            marker.info.open(map, marker);
         };
     }
 
-    function mouseOutHandler(markerForLoop) {
+    function mouseOutHandler(marker) {
         return function mouseOverHandlerReturn() {
-            markerForLoop.info.close();
+            marker.info.close();
         };
     }
 
-    
     // tools
     function log(stuff) {
         console.log(stuff);
     }
 }());
-
-
-//"features": [
-//    {
-//      "type": "Feature",
-//      "id": "TAXIOGD.1",
-//      "geometry": {
-//        "type": "LineString",
-//        "coordinates": [
-//          [
-//            16.48414601729984,
-//            48.239157877962974
-//          ],
-//          [
-//            16.48428803499524,
-//            48.23892437153532
-//          ]
-//        ]
-//      },
-//      "geometry_name": "SHAPE",
-//      "properties": {
-//        "OBJECTID": 1,
-//        "ADRESSE": "22., Zanggasse (Bereich Quadenstraße)",
-//        "ZEITRAUM": null,
-//        "STELLPLATZANZAHL": 5,
-//        "SE_ANNO_CAD_DATA": null
-//      }
-//    },
